dccf9bcfceff1c49f441bbdcf95c6152999e1b40
DuckOfDoom <horaxlfa@gmail.com>
2015-04-11 18:37:14 +0300

    Improved TestBehaviour

--- Assets/Scripts/TestBehaviour.cs
+++ Assets/Scripts/TestBehaviour.cs
@@ -13,18 +13,21 @@ public class TestBehaviour : MonoBehaviour
 
 	private void OnGUI()
 	{
-		_behaviour.URL = GUILayout.TextField(_behaviour.URL);
+		var width = GUILayout.Width(Screen.width / 2f);
+		var height = GUILayout.Height(Screen.height / 5f);
 
-		if (GUILayout.Button("LOG"))
+		_behaviour.URL = GUILayout.TextField(_behaviour.URL, width, height);
+
+		if (GUILayout.Button("LOG", width, height))
 			Debug.Log("Log");
 
-		if (GUILayout.Button("Warning"))
+		if (GUILayout.Button("Warning", width, height))
 			Debug.LogWarning("Warning...");
 
-		if (GUILayout.Button("Error"))
+		if (GUILayout.Button("Error", width, height))
 			Debug.LogError("Error!");
 
-		if (GUILayout.Button("EXCEPTION"))
+		if (GUILayout.Button("EXCEPTION", width, height))
 			Debug.LogException(new ArgumentOutOfRangeException("YOUR MOM"));
 	}
 }
